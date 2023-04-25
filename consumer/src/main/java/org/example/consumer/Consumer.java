3c9a73380e4370efc4ed67e9e1453b974c014bf5
RehasLiamsi <nsaher91@gmail.com>
2023-04-25 22:03:06 +0200

    Create Consumer.java

---
+++ consumer/src/main/java/org/example/consumer/Consumer.java
@@ -0,0 +1,11 @@
+package org.example.consumer;
+
+import org.example.service.WeightConversion;
+
+public class Consumer {
+
+    public static void main(String[] args) {
+
+        WeightConversion weightConversion;
+    }
+}
